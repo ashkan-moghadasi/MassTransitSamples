d18621accb63d8495ba85286a4f04f7cc1522db0
Ashkan <Ashkan@DESKTOP-Q7SLNT3>
2022-06-13 20:48:02 +0430

    Add RabbitMQ2 Sample

---
+++ src/RabbitMQ2/GettingStarted/Contracts/HelloMessage.cs
@@ -0,0 +1,7 @@
+namespace GettingStarted.Contracts
+{
+    public record HelloMessage
+    {
+        public string Name { get; init; }
+    }
+}
---
+++ src/RabbitMQ2/GettingStarted/Worker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GettingStarted.Contracts;
+using MassTransit;
+using Microsoft.Extensions.Hosting;
+
+namespace GettingStarted
+{
+    public class Worker : BackgroundService
+    {
+        private IBus bus;
+
+        public Worker(IBus bus)
+        {
+            this.bus = bus;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await bus.Publish(new HelloMessage()
+                {
+                    Name = "Hello World !"
+                },stoppingToken);
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
